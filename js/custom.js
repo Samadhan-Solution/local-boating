b7e3ee87a7ad50c4698a663584b1a197b59d8961
fazlur <fazlu@samadhan.com.au>
2024-09-19 10:01:24 +1000

    Plugin pushed to share

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,7 @@
 function hideSubmitButton() {
 
     var ready_to_submit = false;
-    //counter++;
-    //if(counter >= 30) clearInterval(refreshIntervalId);
-    //console.log("hideSubmitButton : " + counter);
-
-    // find the total score max
     var total_scores = querySelectorAllInIframes('div.h5p-interactive-book-summary-score-progress  span.absolute-value');
-    //console.log(total_scores);
 
     for (const score of total_scores) {
         var score_got = score[0].innerText;
@@ -18,22 +12,19 @@ function hideSubmitButton() {
         }
 
     var elements = querySelectorAllInIframes('.h5p-interactive-book-summary-submit');
-   // console.log(elements);
 
     for (const button of elements) {
         if(ready_to_submit == false) {
             button[0].style.display = 'none';
-        }
+            }
         else{
             button[0].style.display = 'inline-flex';
+            }
         }
-
-    }
-
     }
 }
 
-//let counter = 0;
+
 var refreshIntervalId = setInterval(hideSubmitButton, 1000);
 hideSubmitButton();
 
@@ -44,16 +35,12 @@ function querySelectorAllInIframes(selector) {
         const iframes = contentWindow.document.body.querySelectorAll('iframe');
         iframes.forEach(iframe => recurse(iframe.contentWindow));
 
-       // console.log("Looking for selector : " + selector);
         const found = contentWindow.document.body.querySelectorAll(selector);
 
-       // console.log(found);
+
         if(found.length > 0){
             elements = elements.concat(found);
         }
-
-        //elements = elements.concat(contentWindow.document.body.querySelectorAll(selector));
-
     }
 
     recurse();
